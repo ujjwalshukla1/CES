cb53af645e90dbd569e0cf0add87e6a29ac31f06
jayraj rathod <jarathod@bestpeers.in>
2026-03-25 17:43:22 +0530

    testimonial section complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import About from "@/components/section/About";
+import TestimonialSection from "@/components/section/Testimonial";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <About />
+      {/* <About /> */}
+      <TestimonialSection />
     </>
   );
 }
